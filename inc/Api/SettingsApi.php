96f6c3ec0ab4606e1bf4616996d32f443a39a7fd
Martin Chorzewski <mchorzewski@whitelabelcoders.com>
2022-06-06 22:04:22 +0200

    Pages & Subpages

--- inc/Api/SettingsApi.php
+++ inc/Api/SettingsApi.php
@@ -1,23 +1,68 @@
 <?php 
 /**
- * @package Touch3D
- */
+* @package Touch3D
+*/
 
 namespace Inc\Api;
 
-use \Inc\Base\BaseController;
-
 /**
- * 
- */
+* 
+*/
 
- class SettingsApi { 
+class SettingsApi { 
 
-	public $this->admin_pages;
+	public $admin_pages    = array();
+	public $admin_subpages = array();
 
-	 public function AddPages( array $pages ) {
+	public function register() {
+		if ( ! empty( $this->admin_pages ) ) {
+			add_action( 'admin_menu', array( $this, 'addAdminMenu' ) );
+		}
+	}
+
+	public function addPages( array $pages ) {
 		$this->admin_pages = $pages;
 
 		return $this;
-	 }
- }
+	}
+
+	public function withSubpage( string $title = null ) {
+		if ( empty( $this->admin_pages ) ) {
+			return $this;
+		}
+
+		$admin_page = $this->admin_pages[0];
+
+		$subpage = array(
+			array(
+				'parent_slug' => $admin_page['menu_slug'],
+				'page_title' => $admin_page['page_title'],
+				'menu_title' => ( $title ) ? $title : $admin_page['menu_title'],
+				'capability' => $admin_page['capability'],
+				'menu_slug'  => $admin_page['menu_slug'],
+				'callback'   => $admin_page['callback']
+			)
+		);
+
+		$this->admin_subpages = $subpage;
+
+		return $this;
+	}
+
+	public function addSubpages( array $pages ) {
+
+		$this->admin_subpages = array_merge( $this->admin_subpages, $pages ) ;
+
+		return $this;
+	}
+
+	public function addAdminMenu() {
+
+		foreach ( $this->admin_pages as $page ) {
+			add_menu_page( $page['page_title'], $page['menu_title'], $page['capability'], $page['menu_slug'], $page['callback'], $page['icon_url'], $page['position'] );
+		}
+		foreach ( $this->admin_subpages as $page ) {
+			add_submenu_page( $page['parent_slug'], $page['page_title'], $page['menu_title'], $page['capability'], $page['menu_slug'], $page['callback'] );
+		}
+	}
+}
--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -6,22 +6,46 @@
 namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
-
+use \Inc\Api\SettingsApi;
 /**
  * 
  */
 
  class Admin extends BaseController {
 
+	public $settings;
+	public $pages = array();
+	public $subpages = array();
+
+	public function __construct() {
+
+		$this->settings = new SettingsApi();
+
+		$this->pages = array(
+			array(
+				'page_title' => 'Touch3D',
+				'menu_title' => 'Touch3D',
+				'capability' => 'manage_options',
+				'menu_slug'  => 'touch3d',
+				'callback'   => function() { echo '<h1>Touch3D</h1>'; },
+				'icon_url'   => 'dashicons-image-filter',
+				'position'   => '110'
+			)
+		);
+
+		$this->subpages = array(
+			array(
+				'parent_slug' => 'touch3d',
+				'page_title' => 'General',
+				'menu_title' => 'General',
+				'capability' => 'manage_options',
+				'menu_slug'  => 'touch3doptions',
+				'callback'   => function() { echo '<h1>General</h1>'; }
+			)
+		);
+	}
+
 	public function register() {
-		add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
-	}
-
-	public function add_admin_pages() {
-		add_menu_page( 'Touch3D', 'Touch3D', 'manage_options', 'touch3d', array( $this, 'admin_index' ), 'dashicons-image-filter', 110 );
-	}
-
-	public function admin_index() {
-		require_once $this->plugin_path . 'templates/admin.php';
+		$this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
 	}
  }
