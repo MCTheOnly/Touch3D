6db5cedaf70438e596d1ae8158a342930264130c
Martin Chorzewski <mchorzewski@whitelabelcoders.com>
2022-06-07 00:52:27 +0200

    Create DB

--- inc/Base/Activate.php
+++ inc/Base/Activate.php
@@ -1,16 +1,35 @@
 <?php 
 /**
- * @package Touch3D
- */
+* @package Touch3D
+*/
 
- namespace Inc\Base;
+namespace Inc\Base;
 
- /**
- * 
- */
+/**
+* 
+*/
 
- class Activate {
-	 public static function activate() {
-		 flush_rewrite_rules();
-	 }
- }
+class Activate {
+	public static function activate() {
+
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name      = $wpdb->prefix . 'touch3d_main';
+
+		// /DEV ENV/ ->
+		$wpdb->query( "DROP TABLE IF EXISTS $table_name" );
+		// <- /DEV ENV/
+	
+		$sql = "CREATE TABLE $table_name (
+			t3d_option_id mediumint(9) NOT NULL AUTO_INCREMENT,
+			t3d_option_name varchar(191) NOT NULL,
+			time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+			UNIQUE KEY id (t3d_option_id)
+		) $charset_collate;";
+	
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
+
+		flush_rewrite_rules();
+	}
+}
